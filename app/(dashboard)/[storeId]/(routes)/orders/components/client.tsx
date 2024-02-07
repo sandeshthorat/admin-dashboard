9aeed156a325d0d3aac7838b990728f701a2e27b
Sandesh Thorat <sandeshthorat13@gmail.com>
2024-02-08 00:56:34 +0530

    admin

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { OrderColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
+
+interface OrdersClientProps {
+    data: OrderColumn[]
+}
+
+export const OrdersClient: React.FC<OrdersClientProps> = ({
+    data
+}) => {
+    return (
+        <>
+            <Heading
+                title={`Orders (${data.length})`}
+                description="Manage order for your store"
+            />
+            <Separator />
+            <DataTable searchKey="proucts" columns={columns} data={data} />
+        </>
+    )
+}
